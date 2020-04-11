245f7976a0a9dd0f66c65e9eadbe1f6a6b825a6e
Scott Brooks <59925973+Sjbrooks86@users.noreply.github.com>
2020-04-11 12:37:09 -0400

    first

---
+++ Movie.Data/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie.Data
+{
+    public class Movie
+    {   
+        [Key]
+        public int MovieId { get; set; }
+        
+        [Required]
+        public string MovieImage { get; set; }
+        
+        [Required]
+        public string MovieName { get; set; }
+        
+        [Required]
+        public string MovieGrenre { get; set; }
+        
+        [Required]
+        public string MovieDescription { get; set; }
+        
+        [Required]
+        public string MovieRating { get; set; }
+        
+        [Required]
+        public Nullable<System.DateTime> MovieTime { get; set; }
+        
+        [Required]
+        public string MovieCast { get; set; }
+    }
+}
