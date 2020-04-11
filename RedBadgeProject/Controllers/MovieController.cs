e29b315c6413f639871754178f99691d8a6f38ad
Scott Brooks <59925973+Sjbrooks86@users.noreply.github.com>
2020-04-11 14:27:18 -0400

    nav bar stuff

--- RedBadgeProject/Controllers/MovieController.cs
+++ RedBadgeProject/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using Movie.Models;
+﻿using Movie.Model;
+using Movie.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,25 @@ namespace RedBadgeProject.Controllers
         public ActionResult Index()
         {
             var model = new MovieListItem[0];
+            return View(model);
+        }
+        //Add method here VVVV
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        
+        //Add code here vvvv
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MovieCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
