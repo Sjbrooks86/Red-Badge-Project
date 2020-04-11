f2559cfb1bf19cd14d2808341e2fb0ce9056d099
Scott Brooks <59925973+Sjbrooks86@users.noreply.github.com>
2020-04-11 14:04:16 -0400

    added controllers

---
+++ RedBadgeProject/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using Movie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedBadgeProject.Controllers
+{
+    [Authorize]
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var model = new MovieListItem[0];
+            return View();
+        }
+    }
+}
